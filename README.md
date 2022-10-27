e6ffbaf7afa03c4d952639ccf4e492893d145392
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-10-27 17:42:19 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# frontend
+# Run app
+
+npm run start
+
+# React version ^18.2.0
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './Global.scss'
+
+function App() {
+  return (
+    <div className='app'>
+    </div>
+  );
+}
+
+export default App;
+
