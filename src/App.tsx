1c43a99a813c7996dc69111c4755a95a0b547c7c
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-10-30 20:56:14 +0200

    was created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import './Global.scss'
+import './Global.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/sign-in';
+import SignUp from './pages/sign-up';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='app'>
-      <Header/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-
