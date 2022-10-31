903d550c1a4c1d2bbebc5b8145fb95d8276f6791
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-10-31 10:50:40 +0200

    was created sign-in/sign-up pages

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import SignInForm from '../components/sign-in-form';
+
+export default function SignIn() {
+  return <SignInForm />;
+}
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,7 @@
+import React, { FC, ReactElement } from 'react';
+
+const SignUp: FC<{}> = (): ReactElement => {
+  return <p>SignUp</p>;
+};
+
+export default SignUp;
