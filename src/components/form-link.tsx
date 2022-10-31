518e0401965e9235c50cb9b95b7651822a791deb
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-10-31 13:17:44 +0200

    add link component

---
+++ src/components/form-link.tsx
@@ -0,0 +1,39 @@
+import React, { FC, ReactElement, useState } from 'react';
+import { Link } from 'react-router-dom';
+import linkStyles from '../styles/link-styles';
+import { Typography } from '@mui/material';
+
+type PropType = {
+  linkConfig: {
+    path: string;
+    title: string;
+    styles?: {};
+  };
+};
+
+const FormLink: FC<PropType> = ({ linkConfig }: PropType): ReactElement => {
+  const [isHover, setIsHover] = useState<boolean>(false);
+
+  return (
+    <Typography sx={{ textAlign: 'center' }}>
+      <Link
+        to={linkConfig.path}
+        style={{
+          ...linkStyles.default,
+          ...linkConfig.styles,
+          ...(isHover ? linkStyles.hover : null),
+        }}
+        onMouseEnter={() => {
+          setIsHover(true);
+        }}
+        onMouseLeave={() => {
+          setIsHover(false);
+        }}
+      >
+        {linkConfig.title}
+      </Link>
+    </Typography>
+  );
+};
+
+export default FormLink;
