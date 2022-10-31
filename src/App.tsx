23048cbd02ddf1e23acaf7568bfc3bfe4b1679da
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-10-31 13:14:29 +0200

    add recover password route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import './Global.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/sign-in';
+import RecoverPassword from './pages/recover-password';
 import SignUp from './pages/sign-up';
 import Header from './components/Header/Header';
+import NoMatchPage from './pages/no-match-page';
 
 function App() {
   return (
@@ -10,8 +13,10 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='sign-in' element={<SignIn />} />
+          <Route path='sign-up' element={<SignUp />} />
+          <Route path='recover-password' element={<RecoverPassword />} />
+          <Route path='*' element={<NoMatchPage />} />
         </Routes>
       </BrowserRouter>
     </div>
