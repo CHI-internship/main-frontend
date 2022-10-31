5edb30a89ccd0c62ebecd58bae781d984294da6b
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-10-31 13:18:32 +0200

    add recover password page

---
+++ src/pages/recover-password.tsx
@@ -0,0 +1,8 @@
+import React, { FC, ReactElement } from 'react';
+import RecoverPasswordForm from '../components/recover-password-form';
+
+const RecoverPassword: FC<{}> = (): ReactElement => {
+  return <RecoverPasswordForm />;
+};
+
+export default RecoverPassword;
