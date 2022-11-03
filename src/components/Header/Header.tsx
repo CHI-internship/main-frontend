0379eea2c1ee0c99e4a3b01c2e1de66b56e59b52
Taras Mysiv <tarasmysiv@gmail.com>
2022-11-03 15:36:33 +0200

    fixed comments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,6 @@ import logo from '../../images/user.png';
 import style from './Header.module.scss';
 
 const Header: FC = () => (
-
   <AppBar position='static' color='primary'>
     <Toolbar>
       <Typography
