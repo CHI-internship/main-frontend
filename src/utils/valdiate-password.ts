5a4b6303c710c30c168c130d68cc0a9005275986
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-11-03 21:30:57 +0200

    remove useless functions

--- src/utils/valdiate-password.ts
+++
@@ -1,4 +0,0 @@
-export const validatePassword = (password: string) => {
-  const regEx = /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-  return regEx.test(password);
-};
--- src/utils/validate-email.ts
+++
@@ -1,4 +0,0 @@
-export const validateEmail = (email: string): boolean => {
-  const regEx = /\S+@\S+\.\S+/;
-  return regEx.test(email);
-};
