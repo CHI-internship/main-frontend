eeb743676b6ab06672867853545d731831625398
Taras Mysiv <tarasmysiv@gmail.com>
2022-11-03 13:46:10 +0200

    fixed comments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,43 @@
-import React, { FC } from 'react';
 import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 
-const Header: FC = () => {
+import logo from '../images/user.png';
+import style from './Header.module.scss';
+
+const Header = () => {
 
   return (
-    <>
-      <AppBar position='static' color='primary'>
-        <Toolbar>
-          <Typography
-            variant='h5'
-            sx={{ flexGrow: 1 }}>
+
+    <AppBar position='static' color='primary'>
+      <Toolbar>
+        <Typography
+          variant='h5'
+          sx={{ flexGrow: 1 }}
+        >
             KraudDonate
-          </Typography>
+        </Typography>
 
-          <Box className={'profile_container'}>
+        <Box className={style.profile_container}>
 
-            <IconButton
-              sx={{ padding:0}}
-              color='inherit'>
-              <PermIdentityIcon />
-            </IconButton>
+          <IconButton
+            sx={{ padding: 0 }}
+            color='inherit'
+          >
+            <PermIdentityIcon />
+          </IconButton>
 
-            <Box className={'avatar'}>
-              <img
-                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4OPBibmFcA0jgIPbN-6wPiy37vbmfQN7SAg&usqp=CAU'
-                alt='avatar' />
-            </Box>
-
-            <p>TM</p>
-
+          <Box className={style.avatar}>
+            <img
+              src={logo}
+              alt='avatar'
+            />
           </Box>
-        </Toolbar>
-      </AppBar>
-    </>
+
+          <p>TM</p>
+
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
