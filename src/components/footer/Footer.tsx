30e7b6f3faf33ebd1c65131836a7d4685949a0ae
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-11-16 18:04:33 +0200

    stick footer to bottom

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,6 +13,7 @@ const Footer: FC = () => {
         justifyContent: 'center',
         backgroundColor: 'silver',
         padding: '0.5rem 1rem 0 1rem',
+        marginTop: 'auto',
       }}
     >
       <Box>
