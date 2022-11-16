d0b96537924c7126bbe437772ea964e2c7ce54ed
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-11-16 17:19:33 +0200

    add limit and page to request

--- src/api/orders.service.ts
+++ src/api/orders.service.ts
@@ -1,11 +1,13 @@
-import { axiosInstance } from "./axios-instance";
+import { axiosInstance } from './axios-instance';
 
 class OrderService {
-    async getOrders() {
-        return await axiosInstance.get('orders')
-            .then(data => data.data).catch(() => [])
-    }
+  async getOrders(limit = 10, page = 1) {
+    return await axiosInstance
+      .get('orders', { params: { limit, page } })
+      .then(data => data.data)
+      .catch(() => []);
+  }
 }
 
-const orderService = new OrderService()
-export default orderService
+const orderService = new OrderService();
+export default orderService;
