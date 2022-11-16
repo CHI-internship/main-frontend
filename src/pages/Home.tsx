48bb81aa9747536cccf5942d617c227246d2a1f0
oleksandr.snytko <oleksandr.snytko@chisw.com>
2022-11-16 17:23:15 +0200

    add popular orders and last reports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
-import ShortAbout from '../components/home/ShortAbout';
-import { Box, Button, Typography } from '@mui/material';
-import GetStarted from '../components/home/GetStarted';
+import ShortAbout from '../components/home/ShortAbout/ShortAbout';
+import { Box, Typography } from '@mui/material';
+import GetStarted from '../components/home/GetStarted/GetStarted';
+import OrderList from '../components/home/OrderList/OrderList';
+import LastReports from '../components/home/LastReports/LastReports';
 
 const Home: FC = () => {
   return (
@@ -17,6 +19,7 @@ const Home: FC = () => {
         >
           Most Popular
         </Typography>
+        <OrderList />
       </Box>
       <Box sx={{ margin: '1rem 0 0 0' }}>
         <Typography
@@ -29,6 +32,18 @@ const Home: FC = () => {
           Do you want to create your project?
         </Typography>
         <GetStarted />
+      </Box>
+      <Box sx={{ margin: '1rem 0 0 0' }}>
+        <Typography
+          sx={{
+            textAlign: 'center',
+            fontSize: '1.25rem',
+            borderBottom: '1px solid black',
+          }}
+        >
+          Last reports
+        </Typography>
+        <LastReports />
       </Box>
     </>
   );
