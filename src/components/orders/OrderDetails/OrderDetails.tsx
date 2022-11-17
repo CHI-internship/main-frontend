d39617c395191d1ecd65a57ea8a8d70181a10dcd
Taras Mysiv <tarasmysiv@gmail.com>
2022-11-17 12:08:00 +0200

    add order page (single) finally

---
+++ src/components/orders/OrderDetails/OrderDetails.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+
+import style from './OrderDetails.module.scss';
+import ProgressBar from '../../progressbar/Progressbar';
+import { IOrder } from '../../../types/order.types';
+
+interface IOrderCardProps {
+  order: IOrder
+}
+const OrderDetails: FC<IOrderCardProps> = ({ order }) => (
+  <div className={style.container}>
+    <div className={style.title}>
+      <h1>{order?.title}</h1>
+      <p>{order?.info}</p>
+    </div>
+    <div className={style.main}>
+      <div className={style.image}
+        style={{ backgroundImage: `url(${order?.photo})` }}>
+      </div>
+      <div className={style.content}>
+        <div className={order?.status === 'open' ? style.gathering_open : style.gathering_close}>
+          <h4>{order?.status === 'open' ? 'Відкритий збір' : 'Збір закрито'}</h4>
+        </div>
+        <ProgressBar moneyHave={order?.sum} moneyNeed={order?.goal_amount}
+          closedAt={new Date(order?.finished_at)} size='large' />
+        <p>{order?.short_info}</p>
+        <button>Підтримати</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default OrderDetails;
