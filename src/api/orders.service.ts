68ac25dd68aef7241d0a11248984152605696fe0
Taras Mysiv <tarasmysiv@gmail.com>
2022-11-27 20:33:13 +0200

    fix name method getUserOrder

--- src/api/orders.service.ts
+++ src/api/orders.service.ts
@@ -1,9 +1,10 @@
 import { axiosInstance } from './axios-instance';
 import { AxiosError, AxiosResponse } from 'axios';
+import { IOrderDto } from '../types';
 
 class OrderService {
   async getOrders(page = 1, limit = 10, sort = 'asc') {
-    return await axiosInstance
+    return axiosInstance
       .get('orders', { params: { limit, page, sort } })
       .then((data: AxiosResponse) => data.data)
       .catch((err: AxiosError) => {
@@ -12,7 +13,7 @@ class OrderService {
   }
 
   async getOrderById(id: string) {
-    return await axiosInstance
+    return axiosInstance
       .get(`orders/${id}`)
       .then((value: AxiosResponse) => value.data)
       .catch((err: AxiosError) => {
@@ -21,13 +22,18 @@ class OrderService {
   }
 
   async updateOrder(id:number,data:any) {
-    return await axiosInstance
+    return axiosInstance
       .patch(`orders/${id}`, data)
       .then(value => value.data)
   }
 
+  async createOrder(orderDto: IOrderDto) {
+    return axiosInstance.post('orders', orderDto)
+      .then(value => value.data).catch(() => false)
+  }
+
   async getUserOrder(id: number, userId: number) {
-    return await axiosInstance
+    return axiosInstance
       .get(`orders/${id}/${userId}`)
       .then(value => value.data)
       .catch((err:AxiosError) => {
